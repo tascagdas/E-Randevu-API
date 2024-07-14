0cd48330c4531047151e7f7ed872e86febb7d71b
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-07-14 16:28:55 +0300

    Application katmanı kuruldu ve Denedencyinjection ile MediatR kütüphanesi kurulumu için ön hazırlık yapıldı.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+        return serviceCollection;
+    }
+}
