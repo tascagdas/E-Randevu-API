148c14ce3df8765233df23caca9d1a2f47e7e6cc
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-07-16 15:43:26 +0300

    Doctors Patients ve appointments'e dbset verildi. ve identity kütüphanesinin kullanmadığımız tabloları pasif yapıldı.ı.

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,5 +10,18 @@ internal class ApplicationDbContext : IdentityDbContext<AppUser,AppRole,Guid>
     public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
         
+    }
+
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Bu yöntem ile identity kütüphanesinin oluşturduğu diğer tabloları pasife çekiyoruz.
+        builder.Ignore<IdentityUserClaim<Guid>>();
+        builder.Ignore<IdentityRoleClaim<Guid>>();
+        builder.Ignore<IdentityUserLogin<Guid>>();
+        builder.Ignore<IdentityUserToken<Guid>>();
     }
 }
